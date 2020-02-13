cc9e1ec3da96a4655aebc7f6cdec21f132a2f37b
drHazRuns <djmhayes@protonmail.com>
2020-02-13 04:44:32 +0000

    add neural_network

--- dataset_creation/randomise_and_split_from_labels.py
+++ dataset_creation/randomise_and_split_from_labels.py
@@ -1,11 +1,12 @@
 import random
 
 # variable to hold name of dataset to be randomised and split
-dataset = 'british_outlets'
+dataset = 'satire_vs_real'
 
 # Open dataset, enumerate with random number and sort in ascending numerical 
 # order
-with open('datasets\\' + dataset + '.txt','r', encoding='utf8') as source:
+with open('datasets\\' + dataset + '\\tweets_and_labels.txt','r',\
+     encoding='utf8') as source:
     data = [ (random.random(), line) for line in source ]
 data.sort()
 
@@ -15,8 +16,8 @@ for _, line in data:
     tweet_and_label = line.rsplit(',', 1)
     tweet = tweet_and_label[0]
     label = tweet_and_label[1]
-    with open('datasets\\' + dataset + '_tweets.txt','a', encoding='utf8')\
+    with open('datasets\\' + dataset + '\\tweets.txt','a', encoding='utf8')\
          as tweet_target:
         tweet_target.write(tweet + ' \n')
-    with open('datasets\\' + dataset + '_labels.txt','a') as label_target:
+    with open('datasets\\' + dataset + '\\labels.txt','a') as label_target:
         label_target.write(label)
